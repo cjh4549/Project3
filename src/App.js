0bca696a5c9fec2f488aebc410b2fa21a9215268
Jun Choi <cjh4549@gmail.com>
2021-10-04 12:15:22 -0400

    removed other elements

--- src/App.js
+++ src/App.js
@@ -7,18 +7,6 @@ function App() {
       <header className="App-header">
         <h1>Netlify test test</h1>
         <p>Here's some more stuff</p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
