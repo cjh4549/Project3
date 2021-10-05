bee9f58d0530d898aae47feffeae4c786c91101a
Jun Choi <cjh4549@gmail.com>
2021-10-04 21:09:11 -0400

    set up react functions and connected ref to realtime db

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,25 @@
-// import realtime from './firebase';
+import realtime from './firebase';
+import {useState, useEffect} from 'react';
+import {ref, onValue} from 'firebase/database'
+
 
 function App() {
+
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+
+    const dbRef = ref(realtime);
+
+    onValue(dbRef, (snapshot) => {
+      console.log(snapshot.val());
+    })
+
+  }, []);
+
   return (
     <div className="App">
-      <h1>Test</h1>
+      <h1>Google Keep Inspired Project</h1>
     </div>
   );
 }
